93c33ab74a74a089f9ba44a1bd5ba5b4b9e38ac6
Habeeb <habeebahmadu1@gmail.com>
2024-09-24 15:41:14 +0100

    feat: configure app swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  // Setup Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Synth Blog API')
+    .setDescription('The base API URL is >>> http://localhost:5000')
+    .setTermsOfService('http://localhost:5000/terms-of-service')
+    .setLicense('MIT License', 'github-link')
+    .addServer('http://localhost:5000')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(5000);
 }
 bootstrap();
